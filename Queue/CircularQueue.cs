8bbd6b2299ad4ef9a940751e5c2c19b34f1f39e9
Cral_Cactus <marian.pb@abv.bg>
2023-10-15 13:34:11 +0300

    Upload

---
+++ Queue/CircularQueue.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+public class CircularQueue<T>
+{
+    private const int DefaultCapacity = 16;
+    private T[] elements;
+    private int startIndex;
+    private int endIndex;
+
+    public int Count { get; private set; }
+
+    public CircularQueue()
+    {
+        elements = new T[DefaultCapacity];
+        startIndex = 0;
+        endIndex = 0;
+        Count = 0;
+    }
+
+    public CircularQueue(int capacity)
+    {
+        elements = new T[capacity];
+        startIndex = 0;
+        endIndex = 0;
+        Count = 0;
+    }
+
+    public void Enqueue(T element)
+    {
+        if (Count >= elements.Length)
+        {
+            Grow();
+        }
+
+        elements[endIndex] = element;
+        endIndex = (endIndex + 1) % elements.Length;
+        Count++;
+    }
+
+    private void Grow()
+    {
+        int newCapacity = elements.Length * 2;
+        T[] newElements = new T[newCapacity];
+        for (int i = 0; i < Count; i++)
+        {
+            newElements[i] = elements[(startIndex + i) % elements.Length];
+        }
+        elements = newElements;
+        startIndex = 0;
+        endIndex = Count;
+    }
+
+    public T Dequeue()
+    {
+        if (Count == 0)
+        {
+            throw new InvalidOperationException("Queue is empty");
+        }
+
+        T dequeuedElement = elements[startIndex];
+        startIndex = (startIndex + 1) % elements.Length;
+        Count--;
+
+        return dequeuedElement;
+    }
+
+    public T[] ToArray()
+    {
+        T[] array = new T[Count];
+        for (int i = 0; i < Count; i++)
+        {
+            array[i] = elements[(startIndex + i) % elements.Length];
+        }
+        return array;
+    }
+}
---
+++ Queue/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Queue
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            CircularQueue<int> queue = new CircularQueue<int>(3);
+
+            // Enqueue elements
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4); // Queue will automatically resize
+
+            // Dequeue elements
+            Console.WriteLine("Dequeued element: " + queue.Dequeue());
+            Console.WriteLine("Dequeued element: " + queue.Dequeue());
+
+            // Enqueue more elements after dequeue
+            queue.Enqueue(5);
+            queue.Enqueue(6);
+
+            // Convert queue to array and print
+            int[] array = queue.ToArray();
+            Console.WriteLine("Queue elements as array:");
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
